b1acf675d9623110d50b84c4a1657141f29d5829
messerc <messer55@gmail.com>
2017-02-17 13:45:21 -0800

    changing svg sizes

--- src/icons/WeatherIcon.jsx
+++ src/icons/WeatherIcon.jsx
@@ -80,7 +80,7 @@ const icons = {
 
 const WeatherIcon = props => (
 	<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-		 width="75px" height="75px" viewBox="0 0 100 100" enableBackground="new 0 0 100 100" >
+		 width="60px" height="60px" viewBox="0 0 100 100" enableBackground="new 0 0 100 100" >
 	<path fillRule="evenodd" clipRule="evenodd" d={icons[props.icon]}/>
 	</svg>
 );
